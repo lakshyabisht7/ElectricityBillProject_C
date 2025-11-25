07bb58cd378fd9b3c031949c6776b2a527e3e29f
lakshyabisht7 <lakshya.bisht111@gmail.com>
2025-11-25 21:43:54 +0530

    Updated Project Overview

--- src/Electricity.c
+++ src/Electricity.c
@@ -3,10 +3,10 @@ PROJECT TOPIC:
 Electricity Bill Generator(with Different slabs)
 
 PROJECT OVERVIEW:
-In this project, based on C programming Language, I am working on generating a realistic Electicity Monthly Bill,as per 
-the Current Tariff and all Other Charges applicable for Residential connection under UPCL in Dehradun, Uttarakhand.
-Apart from calculating slab-wise energy charges, the program also stores customer details, allows searching, and displays 
-a formatted bill. File handling is used so that customer records remain saved even after the program is closed.
+In this project, based on C programming Language, I am working on generating a realistic Electicity Monthly Bill, as per the Current Tariff 
+and all Other Charges applicable for Residential connection under UPCL in Dehradun, Uttarakhand. Apart from calculating slab-wise energy charges,
+the program also stores customer details, allows searching, and displays a formatted bill. File handling is used so that customer records 
+remain saved even after the program is closed and opened again. Basic search options (by ID and by name) are also provided for ease of usage.
 
 */
 
